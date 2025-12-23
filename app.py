2ae324ab88df31bf86d2dbb603e25d49ac987cad
sheikhalyan <alyantaj789@gmail.com>
2025-12-23 10:48:18 +0500

    remove all deployments changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-#from waitress import serve -- commented for railway deployment
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -786,10 +786,9 @@ def femur_page():
     machine = request.args.get('machine', 'default_machine_value')
     return render_template('femur.html', machine=machine)
 
-#commented for Railway Deployment
-#if __name__ == '__main__':
-#    serve(app, host='0.0.0.0', port=5000)  # listen on all interfaces
 
 if __name__ == '__main__':
-    app.run()
+    serve(app, host='0.0.0.0', port=5000)  # listen on all interfaces
+
+
 
